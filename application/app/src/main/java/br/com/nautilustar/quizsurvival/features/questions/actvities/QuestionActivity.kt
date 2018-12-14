6b49ea83f0f604f1acb2b39fbe35d40401113355
Charleston Anjos <charleston.anjos@gmail.com>
2018-12-14 09:50:26 -0200

    Adiciona template de contrato para viewModel

--- application/app/src/main/java/br/com/nautilustar/quizsurvival/features/questions/actvities/QuestionActivity.kt
+++ application/app/src/main/java/br/com/nautilustar/quizsurvival/features/questions/actvities/QuestionActivity.kt
@@ -1,5 +1,6 @@
 package br.com.nautilustar.quizsurvival.features.questions.actvities
 
+import android.os.Bundle
 import androidx.lifecycle.ViewModelProviders
 import br.com.nautilustar.core.base.BaseActivity
 import br.com.nautilustar.quizsurvival.databinding.ActivityQuestionBinding
@@ -7,6 +8,11 @@ import br.com.nautilustar.quizsurvival.R
 import br.com.nautilustar.quizsurvival.features.questions.viewmodel.QuestionViewModel
 
 class QuestionActivity : BaseActivity<ActivityQuestionBinding, QuestionViewModel>() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getViewModel().input.start()
+    }
 
     override fun getLayoutId(): Int {
         return R.layout.activity_question
--- application/app/src/main/java/br/com/nautilustar/quizsurvival/features/questions/viewmodel/QuestionViewModel.kt
+++ application/app/src/main/java/br/com/nautilustar/quizsurvival/features/questions/viewmodel/QuestionViewModel.kt
@@ -1,7 +1,36 @@
 package br.com.nautilustar.quizsurvival.features.questions.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import br.com.nautilustar.core.base.BaseViewModel
 import javax.inject.Inject
 
-class QuestionViewModel @Inject constructor() : BaseViewModel() {
+interface InputQuestionViewModel {
+    fun start()
+}
+
+interface OutputQuestionViewModel {
+    val show: LiveData<Boolean>
+}
+
+interface ContractQuestionViewModel {
+    val input: InputQuestionViewModel
+    val output: OutputQuestionViewModel
+}
+
+class QuestionViewModel @Inject constructor() : BaseViewModel(),
+    ContractQuestionViewModel,
+    InputQuestionViewModel,
+    OutputQuestionViewModel {
+
+    private val showObservable = MutableLiveData<Boolean>()
+
+    override val input: InputQuestionViewModel get() = this
+    override val output: OutputQuestionViewModel get() = this
+
+    override val show: LiveData<Boolean> get() = showObservable
+
+    override fun start() {
+        showObservable.value = true
+    }
 }
