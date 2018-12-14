03ca62de4731bdb5c377e86249cf73a09632c64e
Charleston Anjos <charleston.anjos@gmail.com>
2018-12-14 10:24:25 -0200

    Mostra fragment

--- application/app/src/main/java/br/com/nautilustar/quizsurvival/features/questions/fragments/QuestionFragment.kt
+++ application/app/src/main/java/br/com/nautilustar/quizsurvival/features/questions/fragments/QuestionFragment.kt
@@ -1,7 +1,15 @@
 package br.com.nautilustar.quizsurvival.features.questions.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class QuestionFragment : Fragment(){
+import br.com.nautilustar.quizsurvival.R
 
+class QuestionFragment : Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_question, container, false)
+    }
 }
